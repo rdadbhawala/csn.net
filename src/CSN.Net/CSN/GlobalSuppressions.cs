1c4986a690786dae0c10e896495c709188a25c75
Rushabh Dadbhawala <rushabh@dadbhawala.in>
2019-04-06 10:48:27 +0530

    Created a basic C# Library Project. Added StyleCop Analyzers

---
+++ src/CSN.Net/CSN/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Abstraction">
+// Copyright (c) Abstraction. All rights reserved.
+// </copyright>
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "Tabs Approved", Scope = "type", Target = "~T:Abstraction.Csn.ICsnWriter")]
---
+++ src/CSN.Net/CSN/ICsnWriter.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ICsnWriter.cs" company="Abstraction">
+// Copyright (c) Abstraction. All rights reserved.
+// </copyright>
+
+namespace Abstraction.Csn
+{
+using System;
+
+	/// <summary>
+	/// ICsnWriter is the raw CSN Writer.
+	/// </summary>
+	public interface ICsnWriter
+    {
+		/// <summary>
+		/// WriteFieldString writes a field with string value.
+		/// </summary>
+		/// <param name="pValue">String value to write.</param>
+		void WriteFieldString(string pValue);
+
+		/// <summary>
+		/// WriteFieldLong writes a field with a long value.
+		/// </summary>
+		/// <param name="pValue">Long value to write.</param>
+		void WriteFieldLong(long pValue);
+
+		/// <summary>
+		/// WriteFieldDateTime writes a field with a DateTime value.
+		/// </summary>
+		/// <param name="pValue">DateTime value to write.</param>
+		void WriteFieldDateTime(DateTime pValue);
+    }
+}
