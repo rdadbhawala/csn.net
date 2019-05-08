04496ec46db78adc3185e9fe009ffa9b2adb8b19
Rushabh Dadbhawala <rushabh@dadbhawala.in>
2019-05-09 04:30:09 +0530

    Added null handling

---
+++ src/CSN.Net/CSN/FieldNull.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="FieldNull.cs" company="Abstraction">
+// Copyright (c) Abstraction. All rights reserved.
+// </copyright>
+
+namespace Abstraction.Csn
+{
+	using System.IO;
+
+	/// <summary>
+	/// Null field
+	/// </summary>
+    internal class FieldNull
+    {
+		public static readonly FieldNull F = new FieldNull();
+
+		private FieldNull()
+		{
+			// nothing
+		}
+
+		/// <summary>
+		/// Write a null field
+		/// </summary>
+		/// <param name="sw">Stream to write unto</param>
+		public void WriteField(StreamWriter sw)
+		{
+			sw.Write(Constants.DefaultFieldSeparator);
+		}
+    }
+}
---
+++ src/CSN.Net/CSN/PrimitiveNull.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="PrimitiveNull.cs" company="Abstraction">
+// Copyright (c) Abstraction. All rights reserved.
+// </copyright>
+
+namespace Abstraction.Csn
+{
+	using System.IO;
+
+	/// <summary>
+	/// Primitive type for Null values
+	/// </summary>
+	public class PrimitiveNull
+		: PrimitiveCast, IPrimitive
+    {
+		/// <summary>
+		/// Singleton
+		/// </summary>
+		public static readonly PrimitiveNull Instance = new PrimitiveNull();
+
+		private PrimitiveNull()
+		{
+			// nothing
+		}
+
+		/// <summary>
+		/// Write a null field.
+		/// </summary>
+		/// <param name="sw">Stream to write unto</param>
+		public override void WritePrimitive(StreamWriter sw)
+		{
+			FieldNull.F.WriteField(sw);
+		}
+	}
+}
